a1be5d9aefb0864d05891ac8627d09ded47b466f
gremble0 <hermanstornes@gmail.com>
2023-08-06 01:33:18 +0200

    Made moving window to different tag also change focus to that tag

--- dwm.c
+++ dwm.c
@@ -1660,6 +1660,7 @@ tag(const Arg *arg)
     if (selmon->sel && arg->ui & TAGMASK) {
         selmon->sel->tags = arg->ui & TAGMASK;
         focus(NULL);
+        view(arg);
         arrange(selmon);
     }
 }
